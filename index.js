e6474555223bf03e560695c6abf26a082d3a185e
Raymond <raymondjohnson@pursuit.org>
2023-03-20 21:37:23 -0400

    Completed project

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+
+//beginning HTML setup
+const header = document.querySelector('h1')
+header.innerText = "Kenti's To Do List";
+const errorMssg = "Please enter a todo!!"
+
+const submitButton = document.querySelector('form')
+
+submitButton.addEventListener('submit', (e) => {
+  e.preventDefault()
+  newListItem(e)
+})
+
+const newListItem = (e) => {
+  const dataSubmitted = document.querySelector('#new-to-do').value 
+  let ultodos = document.querySelector('ul')
+  
+  if(dataSubmitted.length === 0) {
+    const paragraph = document.querySelector('p')
+    paragraph.innerText = errorMssg
+  } else {
+    
+    let todoListItem = document.createElement('li')
+    todoListItem.textContent = dataSubmitted
+    ultodos.append(todoListItem)
+  
+    let currentListItems = document.querySelectorAll('li')
+    currentListItems.innerText = dataSubmitted;
+    todoListItem.addEventListener('click',(event) => {
+      todoListItem.style.textDecoration = 'line-through'
+    })
+  }
+
+}
